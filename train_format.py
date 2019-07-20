278ba2fc4c2c3a2f5b13fdbb730c762ae484f792
Cristi <33129070+cristyioan2000@users.noreply.github.com>
2019-07-20 11:29:54 +0300

    Update train_format.py

--- train_format.py
+++ train_format.py
@@ -1,16 +1,19 @@
 import os
 
 
-
+# Path to Linux
 path = 'build/darknet/x64/data/obj/'
+# Path to Windows // JUST FOR WINDOWS TRAINING
 windows_path = 'data\\obj\\'
 
 # Windows
 # imgList = os.listdir(r'C:\Users\Cristi\Desktop\darknet-master\darknet-master\build\darknet\x64\data\obj')
 # imgList = os.listdir(r'C:\Users\Cristi\Desktop\darknet-master\darknet-master\build\darknet\x64\data\obj')
 
+# Path to obj (train img paths and labels file )
+file_list_path = r''
 #Linux
-imgList = os.listdir(r'D:\Projects\Licenta\Siemens_birou\obj')
+imgList = os.listdir(file_list_path)
 textFile = open('train.txt','w')
 for img in imgList:
     if 'jpg' in img.split('.')[1]:
